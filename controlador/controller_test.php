5c00f2b4ee756497cea7d7f62abc2f08157114ff
DuvanR <dfrobayo9@misena.edu.co>
2022-03-16 15:32:54 -0500

    practice in job

--- controlador/controller_test.php
+++ controlador/controller_test.php
@@ -2,3 +2,23 @@
 function orange (){
     $soso = 322;
 }
+function redline (){
+    $soso = 322;
+}
+
+if($you and $me){
+    $me.happy('S2');
+    echo('ilove you bb');
+    return;
+}elseif($me and $you==0){
+    $me.sad('me');
+}else if($you and $me==0){
+    $you = "understand";
+}
+
+function happy ($person){
+    $person = " :) :) ";
+}
+function sad ($person){
+    $person = " :( :( ";
+}
